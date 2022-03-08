8233b9936346c41e43c2ddae9685ba5d015d1adc
snljty <snljty@gmail.com>
2022-03-08 12:29:52 +0800

    add command argument pharser

--- Makefile
+++ Makefile
@@ -15,50 +15,50 @@ all: rand_rot_trans
 
 rand_rot_trans: rand_rot_trans.exe
 
-rand_rot_trans.exe: rand_rot_trans.o molecule_operation.o quaternion.o \
-vector_operation.o progress_bar.o rand_init.o constant.o
+rand_rot_trans.exe: rand_rot_trans.obj molecule_operation.obj quaternion.obj \
+vector_operation.obj progress_bar.obj rand_init.obj constant.obj
 	@echo Linking $@ ...
 	$(FLINKER) -o $@ $^ -static
 
-rand_rot_trans.o: rand_rot_trans.f90 molecule_module.mod \
+rand_rot_trans.obj: rand_rot_trans.f90 molecule_module.mod \
 quaternion_module.mod progress_bar_module.mod rand_init_module.mod
 	@echo Compiling $@ ...
 	$(FC) -o $@ -fno-realloc-lhs -c $<
 	rem $(FC) -o $@ -c $<
 
-molecule_module.mod: molecule_operation.o
+molecule_module.mod: molecule_operation.obj
 
-molecule_operation.o: molecule_operation.f90 quaternion_module.mod
+molecule_operation.obj: molecule_operation.f90 quaternion_module.mod
 	@echo Compiling $@ ...
 	$(FC) -o $@ -c $<
 
-quaternion_module.mod: quaternion.o
+quaternion_module.mod: quaternion.obj
 
-quaternion.o: quaternion.f90 vector_operation_module.mod constant_module.mod
+quaternion.obj: quaternion.f90 vector_operation_module.mod constant_module.mod
 	@echo Compiling $@ ...
 	$(FC) -o $@ -c $<
 
-vector_operation_module.mod: vector_operation.o 
+vector_operation_module.mod: vector_operation.obj 
 
-vector_operation.o: vector_operation.f90 constant_module.mod
+vector_operation.obj: vector_operation.f90 constant_module.mod
 	@echo Compiling $@ ...
 	$(FC) -o $@ -c $<
 
-progress_bar_module.mod: progress_bar.o
+progress_bar_module.mod: progress_bar.obj
 
-progress_bar.o: progress_bar.f90
+progress_bar.obj: progress_bar.f90
+	@echo Compiling $@ ...
+	$(FC) -o $@ -c $< -fallow-invalid-boz
+
+rand_init_module.mod: rand_init.obj
+
+rand_init.obj: rand_init.f90
 	@echo Compiling $@ ...
 	$(FC) -o $@ -c $<
 
-rand_init_module.mod: rand_init.o
+constant_module.mod: constant.obj
 
-rand_init.o: rand_init.f90
-	@echo Compiling $@ ...
-	$(FC) -o $@ -c $<
-
-constant_module.mod: constant.o
-
-constant.o: constant.f90
+constant.obj: constant.f90
 	@echo Compiling $@ ...
 	$(FC) -o $@ -c $<
 
@@ -70,11 +70,11 @@ clean: clean_tmp
 .PHONY: clean_tmp
 
 clean_tmp:
-	-del constant_module.mod constant.o 1> NUL 2> NUL
-	-del rand_init_module.mod rand_init.o 1> NUL 2> NUL
-	-del progress_bar_module.mod progress_bar.o 1> NUL 2> NUL
-	-del quaternion_module.mod quaternion.o 1> NUL 2> NUL
-	-del vector_operation_module.mod vector_operation.o 1> NUL 2> NUL
-	-del molecule_module.mod molecule_operation.o 1> NUL 2> NUL
-	-del rand_rot_trans.o 1> NUL 2> NUL
+	-del constant_module.mod constant.obj 1> NUL 2> NUL
+	-del rand_init_module.mod rand_init.obj 1> NUL 2> NUL
+	-del progress_bar_module.mod progress_bar.obj 1> NUL 2> NUL
+	-del quaternion_module.mod quaternion.obj 1> NUL 2> NUL
+	-del vector_operation_module.mod vector_operation.obj 1> NUL 2> NUL
+	-del molecule_module.mod molecule_operation.obj 1> NUL 2> NUL
+	-del rand_rot_trans.obj 1> NUL 2> NUL
 
--- rand_rot_trans.f90
+++ rand_rot_trans.f90
@@ -7,59 +7,247 @@ program main
     ! use ieee_arithmetic
     implicit none
     integer(kind=4) :: argc
-    character(kind=1,len=512) :: argv0
     character(kind=1,len=256), allocatable :: argv(:)
+    integer(kind=4) :: iarg
     integer(kind=4) :: arg_status
     character(kind=1,len=256) :: ifile_name, ofile_name
     type(molecule) :: mol_in, mol_out, mol_tmp
     integer(kind=4), parameter :: write_to_unit = 11
-    integer(kind=4) :: write_file_status
+    real(kind=8) :: x_min, x_max, y_min, y_max, z_min, z_max
+    real(kind=8) :: tol_min, tol_max
+    integer(kind=4) :: write_file_status, read_str_status
     integer(kind=4) :: num_gene
     integer(kind=4) :: tried
-    integer(kind=4) :: max_gene
+    integer(kind=4) :: tot_gene
     real(kind=8) :: time_start, time_end
-    ! real(kind=8), parameter :: trans_min_len = 0.0D0, trans_max_len = 8.0D0
+    logical :: is_translate_to_origin
     integer(kind=4) :: i, j
 
     ! get command arguments, not argc in C, this is the argc except argv0.
     argc = command_argument_count()
-    call get_command_argument(0, argv0, arg_status)
-    if (argc > 0) then
-        allocate(argv(argc))
-        do i = 1, argc
-            call get_command_argument(i, argv(i), arg_status)
-        end do
-    end if
+    allocate(argv(0:argc))
+    do iarg = 0, argc
+        call get_command_argument(iarg, argv(iarg), arg_status)
+    end do
 
-    ! currently only uses the first argument as max_gene.
-    if (argc > 0) then
-        read(argv(1), *) max_gene
-    else
-        max_gene = 100
+    ! set default arguments
+    ifile_name = "pentacene.xyz"
+    ofile_name = "traj.xyz"
+    x_min = 3.0D0
+    x_max = 5.0D0
+    y_min = 0.0D0
+    y_max = 1.8125D1
+    z_min = 0.0D0
+    z_max = 9.006D0
+    tol_min = 3.0D0
+    tol_max = 5.5D0
+    tot_gene = 100
+    is_translate_to_origin = .false.
+    
+    ! pharse command arguments
+    iarg = 1
+    if (argv(iarg) == "--help" .or. argv(iarg) == "-h" .or. argv(iarg) == "/?") then
+        write(*, "(a,1x,a)") "Usage:", trim(argv(0))
+        write(*, "(4x,a)") "-h | --help | /?       : print this message and exit."
+        write(*, "(4x,a)") "--ifile-name IFILE_NAME: input file name."
+        write(*, "(4x,a)") "--ofile-name OFILE_NAME: output file name."
+        write(*, "(4x,a)") "--x-min      X_MIN     : minimum translation distance along axis x."
+        write(*, "(4x,a)") "--x-max      X_MAX     : maximum translation distance along axis x."
+        write(*, "(4x,a)") "--y-min      Y_MIN     : minimum translation distance along axis y."
+        write(*, "(4x,a)") "--y-max      Y_MAX     : maximum translation distance along axis y."
+        write(*, "(4x,a)") "--z-min      Z_MIN     : minimum translation distance along axis z."
+        write(*, "(4x,a)") "--z-max      Z_MAX     : maximum translation distance along axis z."
+        write(*, "(4x,a)") "--tol-min    TOL_MIN   : minimum tolerance of distance between closest atoms."
+        write(*, "(4x,a)") "--tol-max    TOL_MAX   : maximum tolerance of distance between closest atoms."
+        write(*, "(4x,a)") "--tot-gene   TOT_GENE  : total amount of molecules in the trajectory."
+        write(*, "(4x,a)") "--to-origin  TO_ORIGIN : whether translate the molecule to the origin point."
+        stop
     end if
+    do while (iarg <= argc)
+        if (argv(iarg) == "--ifile-name") then
+            iarg = iarg + 1
+            if (iarg > argc) then
+                write(0, "(a,a,a)") "Error! Cannot get the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+            read(argv(iarg), *, iostat = read_str_status) ifile_name
+            if (read_str_status /= 0) then
+                write(0, "(a,a,a)") "Error! Cannot pharse the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+        else if (argv(iarg) == "--ofile-name") then
+            iarg = iarg + 1
+            if (iarg > argc) then
+                write(0, "(a,a,a)") "Error! Cannot get the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+            read(argv(iarg), *, iostat = read_str_status) ofile_name
+            if (read_str_status /= 0) then
+                write(0, "(a,a,a)") "Error! Cannot pharse the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+        else if (argv(iarg) == "--x-min") then
+            iarg = iarg + 1
+            if (iarg > argc) then
+                write(0, "(a,a,a)") "Error! Cannot get the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+            read(argv(iarg), *, iostat = read_str_status) x_min
+            if (read_str_status /= 0) then
+                write(0, "(a,a,a)") "Error! Cannot pharse the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+        else if (argv(iarg) == "--x-max") then
+            iarg = iarg + 1
+            if (iarg > argc) then
+                write(0, "(a,a,a)") "Error! Cannot get the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+            read(argv(iarg), *, iostat = read_str_status) x_max
+            if (read_str_status /= 0) then
+                write(0, "(a,a,a)") "Error! Cannot pharse the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+        else if (argv(iarg) == "--y-min") then
+            iarg = iarg + 1
+            if (iarg > argc) then
+                write(0, "(a,a,a)") "Error! Cannot get the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+            read(argv(iarg), *, iostat = read_str_status) y_min
+            if (read_str_status /= 0) then
+                write(0, "(a,a,a)") "Error! Cannot pharse the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+        else if (argv(iarg) == "--y-max") then
+            iarg = iarg + 1
+            if (iarg > argc) then
+                write(0, "(a,a,a)") "Error! Cannot get the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+            read(argv(iarg), *, iostat = read_str_status) y_max
+            if (read_str_status /= 0) then
+                write(0, "(a,a,a)") "Error! Cannot pharse the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+        else if (argv(iarg) == "--z-min") then
+            iarg = iarg + 1
+            if (iarg > argc) then
+                write(0, "(a,a,a)") "Error! Cannot get the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+            read(argv(iarg), *, iostat = read_str_status) z_min
+            if (read_str_status /= 0) then
+                write(0, "(a,a,a)") "Error! Cannot pharse the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+        else if (argv(iarg) == "--z-max") then
+            iarg = iarg + 1
+            if (iarg > argc) then
+                write(0, "(a,a,a)") "Error! Cannot get the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+            read(argv(iarg), *, iostat = read_str_status) z_max
+            if (read_str_status /= 0) then
+                write(0, "(a,a,a)") "Error! Cannot pharse the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+        else if (argv(iarg) == "--tol-min") then
+            iarg = iarg + 1
+            if (iarg > argc) then
+                write(0, "(a,a,a)") "Error! Cannot get the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+            read(argv(iarg), *, iostat = read_str_status) tol_min
+            if (read_str_status /= 0) then
+                write(0, "(a,a,a)") "Error! Cannot pharse the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+        else if (argv(iarg) == "--tol-max") then
+            iarg = iarg + 1
+            if (iarg > argc) then
+                write(0, "(a,a,a)") "Error! Cannot get the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+            read(argv(iarg), *, iostat = read_str_status) tol_max
+            if (read_str_status /= 0) then
+                write(0, "(a,a,a)") "Error! Cannot pharse the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+        else if (argv(iarg) == "--tot-gene") then
+            iarg = iarg + 1
+            if (iarg > argc) then
+                write(0, "(a,a,a)") "Error! Cannot get the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+            read(argv(iarg), *, iostat = read_str_status) tot_gene
+            if (read_str_status /= 0) then
+                write(0, "(a,a,a)") "Error! Cannot pharse the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+        else if (argv(iarg) == "--to-origin") then
+            iarg = iarg + 1
+            if (iarg > argc) then
+                write(0, "(a,a,a)") "Error! Cannot get the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+            if (argv(iarg) == "f" .or. argv(iarg) == ".f." .or. argv(iarg) == "false" .or. argv(iarg) == ".false." .or. &
+                argv(iarg) == "F" .or. argv(iarg) == ".F." .or. argv(iarg) == "False" .or. argv(iarg) == ".False.") then
+                is_translate_to_origin = .false.
+            else if (argv(iarg) == "t" .or. argv(iarg) == ".t." .or. argv(iarg) == "true" .or. argv(iarg) == ".true." .or. &
+                argv(iarg) == "T" .or. argv(iarg) == ".T." .or. argv(iarg) == "True" .or. argv(iarg) == ".True.") then
+                is_translate_to_origin = .true.
+            else
+                write(0, "(a,a,a)") "Error! Cannot pharse the argument for """, trim(argv(iarg - 1)), """."
+                stop 1
+            end if
+        else
+            write(0, "(a,a,a)") "Error! Cannot understand argument """, trim(argv(iarg)), """."
+            stop 1
+        end if
+        iarg = iarg + 1
+    end do
+
+    write(*, "(a,1x,a)") "IFILE_NAME:", trim(ifile_name)
+    write(*, "(a,1x,a)") "OFILE_NAME:", trim(ofile_name)
+    write(*, "(a,1x,f6.3)") "X_MIN     :", x_min
+    write(*, "(a,1x,f6.3)") "X_MAX     :", x_max
+    write(*, "(a,1x,f6.3)") "Y_MIN     :", y_min
+    write(*, "(a,1x,f6.3)") "Y_MAX     :", y_max
+    write(*, "(a,1x,f6.3)") "Z_MIN     :", z_min
+    write(*, "(a,1x,f6.3)") "Z_MAX     :", z_max
+    write(*, "(a,1x,f6.3)") "TOL_MIN   :", tol_min
+    write(*, "(a,1x,f6.3)") "TOL_MAX   :", tol_max
+    write(*, "(a,1x,i0)") "TOT_GENE  :", tot_gene
+    write(*, "(a,1x)", advance = "no") "TO_ORIGIN :"
+    if (is_translate_to_origin) then
+        write(*, "(a)") "YES"
+    else
+        write(*, "(a)") "NO"
+    end if
+    write(*, "()")
 
     ! release argv
-    if (argc > 0) deallocate(argv)
+    deallocate(argv)
 
     ! something like "call random_seed()"
     call random_init_seed()
 
     call cpu_time(time_start)
 
-    ! write(*, "(A)") "Input file name for reading: "
-    ! read(*, "(A)") ifile_name
-    ifile_name = "pentacene.xyz"
-
     mol_in = read_xyz(ifile_name)
-    ! the input file "pentacene.xyz" is already located its geometry center
+    ! if the input file is already located its geometry center
     ! at the origin of the coordinates, hence no need to translate it to origin.
-    ! translate_molecule_to_origin(mol_in)
+    if (is_translate_to_origin) then
+        call translate_molecule_to_origin(mol_in)
+        open(write_to_unit, file = trim(ifile_name), status = "replace", action = "write", &
+            position = "rewind", iostat = write_file_status)
+        call write_xyz(mol_in, write_to_unit)
+        close(write_to_unit)
+    end if
 
-    ! write(*, "(A)") "Input file name for writing: "
-    ! read(*, "(A)") ofile_name
-    ofile_name = "traj.xyz"
     open(write_to_unit, file = trim(ofile_name), status = "replace", &
-         access = "sequential", form = "formatted", action = "write", iostat = write_file_status)
+         action = "write", iostat = write_file_status)
 
     if (write_file_status /= 0) then
         write(*, "(a,a,a)") "File """, trim(ofile_name), """ cannot be created!"
@@ -67,25 +255,23 @@ program main
     end if
 
     tried = 0
-    loop_rotate: do num_gene = 1, max_gene
+    loop_rotate: do num_gene = 1, tot_gene
         mol_tmp = mol_in
         ! call rotate_molecule_random(mol_tmp)
         call rotate_molecule_random(mol_tmp)
         ! rotate once, then translate randomly until suits the threshold condition.
         loop_translate: do while(.true.)
             mol_out = mol_tmp
-            call translate_molecule_random(mol_out, 3.0D0, 5.0D0, &
-                                                    0.0D0, 18.125D0, &
-                                                    0.0D0, 9.006D0)
+            call translate_molecule_random(mol_out, x_min, x_max, y_min, y_max, z_min, z_max)
             tried = tried + 1
-            if (check_minimal_distance_acceptable(mol_in, mol_out, 3.0D0, 5.5D0)) exit            
+            if (check_minimal_distance_acceptable(mol_in, mol_out, tol_min, tol_max)) exit            
         end do loop_translate
         ! write(*, "(f10.6)") get_minimal_distance(mol_in, mol_out)
         call write_xyz(mol_out, write_to_unit)
-        call print_progress_bar(dble(num_gene) / dble(max_gene))
+        call print_progress_bar(dble(num_gene) / dble(tot_gene))
     end do loop_rotate
 
-    write (*, "(/,a,i8,a,i8,a)") "Tried ", tried, " times, generated ", max_gene, " ."
+    write (*, "(/,a,i8,a,i8,a)") "Tried ", tried, " times, generated ", tot_gene, " ."
 
     close(write_to_unit)
 
